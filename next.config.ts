14982d0472837af75be98f6a27f583637674eb57
RamboGj <jpramboguanabara@hotmail.com>
2025-04-22 13:58:23 -0300

    done

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+        {
+            source: '/(.*)?', // Matches all pages
+            headers: [
+                {
+                    key: 'X-Frame-Options',
+                    value: 'DENY',
+                }
+            ]
+        }
+    ]
+}
   /* config options here */
 };
 
