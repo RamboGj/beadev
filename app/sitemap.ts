300280895c54ca56e7c1cd8a7775d4e54d2f8b02
RamboGj <jpramboguanabara@hotmail.com>
2025-04-22 23:19:57 -0300

    return sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,18 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	const languages = ["pt-BR", "en-US"]; // Adicione mais idiomas conforme necessário
-
-	return languages.flatMap((lang) => [
+	return [
 		{
-			url: `https://www.beadev.com.br/${lang}`,
+			url: "https://www.beadev.com.br/",
 			lastModified: new Date(),
 			changeFrequency: "weekly",
-			priority: 1.0,
-			alternateRefs: languages
-				.filter((l) => l !== lang)
-				.map((l) => `https://www.beadev.com.br/${l}`),
+			priority: 1,
+			alternates: {
+				languages: {
+					en: "https://www.beadev.com.br/en-US",
+					br: "https://www.beadev.com.br/pt-BR",
+				},
+			},
 		},
-	]);
+	];
 }
