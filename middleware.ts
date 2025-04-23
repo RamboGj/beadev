5ed440c6c4d88a8867ab2c297f64f53926f1f9c0
RamboGj <jpramboguanabara@hotmail.com>
2025-04-23 10:40:00 -0300

    fix: remove CSP

--- middleware.ts
+++ middleware.ts
@@ -48,18 +48,22 @@ export function middleware(request: NextRequest) {
 	}
 
 	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+	// 	const cspHeader = `
+	//     default-src 'self';
+	//     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+	//     style-src  'unsafe-inline' 'self';
+	//     img-src 'self' blob: data:;
+	//     font-src 'self' data:;
+	//     object-src 'none';
+	//     base-uri 'self';
+	//     form-action 'self';
+	//     frame-ancestors 'none';
+	//     upgrade-insecure-requests;
+	// `;
+
 	const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src  'unsafe-inline' 'self';
-    img-src 'self' blob: data:;
-    font-src 'self' data:;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
+    
+ `;
 	// Replace newline characters and spaces
 	const contentSecurityPolicyHeaderValue = cspHeader
 		.replace(/\s{2,}/g, " ")
