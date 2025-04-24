4e092c3e4cf6e50ff8dbd4202a67bc185316bfbb
RamboGj <jpramboguanabara@hotmail.com>
2025-04-23 23:34:14 -0300

    test

--- app/[lang]/[slug]/page.tsx
+++ app/[lang]/[slug]/page.tsx
@@ -46,6 +46,8 @@ export default async function BlogPostPage({
 	params,
 }: { params: Promise<{ lang: string; slug: string }> }) {
 	const { lang, slug } = await params;
+	console.log("lang ->", lang);
+	console.log("slug ->", slug);
 
 	const article: ArticleProps = await getArticle({ slug, locale: lang });
 
