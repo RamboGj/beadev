2fbcdb280e550283c8236227333445f4138ec070
RamboGj <jpramboguanabara@hotmail.com>
2025-08-06 12:39:42 -0300

    without user id on config

--- app/lib/ga/index.tsx
+++ app/lib/ga/index.tsx
@@ -4,21 +4,21 @@ import { useEffect } from "react";
 import { env } from "../env";
 import { sendGAEvent } from "@next/third-parties/google";
 
-const MOCK_USER_ID = "user-0101-USING-sendGAEvent()";
+const MOCK_USER_ID = "user-0101-WITHOUT_USER_iD_ON_CONFIG()";
 
 export function EnsureGAInit() {
 	useEffect(() => {
 		if (typeof window !== "undefined" && window.gtag) {
 			console.log("window.tag", window.gtag);
 
-			// window.gtag("set", { user_id: MOCK_USER_ID });
+			window.gtag("set", { user_id: MOCK_USER_ID });
 
-			sendGAEvent("set", { user_id: MOCK_USER_ID });
-			sendGAEvent("config", env.NEXT_PUBLIC_GA_ID);
+			// sendGAEvent("set", { user_id: MOCK_USER_ID });
+			// sendGAEvent("config", env.NEXT_PUBLIC_GA_ID);
 
-			// window.gtag("config", env.NEXT_PUBLIC_GA_ID || "", {
-			// 	user_id: MOCK_USER_ID,
-			// });
+			window.gtag("config", env.NEXT_PUBLIC_GA_ID || "", {
+				user_id: MOCK_USER_ID,
+			});
 		}
 	}, []);
 
